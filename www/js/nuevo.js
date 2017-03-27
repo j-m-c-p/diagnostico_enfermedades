8644e9fc3d796e37faf204c0c616cf940d4c5482
j-m-c-p <jhonatancubi@gmail.com>
2017-03-27 10:44:05 -0500

    Update nuevo.js

--- www/js/nuevo.js
+++ www/js/nuevo.js
@@ -1,82 +1,59 @@
 var acumuladorApp = angular.module( 'acumuladorApp', [] );
 acumuladorApp.controller( "acumuladorAppCtrl",
-//[ "$scope",  //Originalmente solo se minificaba el scope.
-[ "$scope", "$http", //Esto es para minificar, pero interfiere con lo de php, hay que minificar las otras variables.
-    function( $scope, $http )
-    {
-        /**
-        * Esta función se activa al usar el texto 2.
-        */
-        $scope.cargar_datos_php = function()
+    
+    //[ "$scope",  //Originalmente solo se minificaba el scope.
+    [ "$scope", "$http", //Esto es para minificar, pero interfiere con lo de php, hay que minificar las otras variables.
+     
+        function( $scope, $http )
         {
-            var lista= document.getElementById('datos');
-            console.log(lista.length );
-            var sintomas="";
-
-
-            for (var i = 0 ; i < lista.length ; i++) 
+        
+            
+            
+            /**
+             * Esta función se activa al usar el texto 2.
+             */
+            $scope.cargar_datos_php = function()
             {
-                if (lista.item(i).selected ) 
-                {
-                    if (sintomas == "")
-                    { 
-                        sintomas +=lista.item(i).value;
-                    }
-                    else{
-                            sintomas +="," +lista.item(i).value;
+                    var lista= document.getElementById('datos');
+                    console.log(lista.length );
+                    var sintomas="";
+
+
+                    for (var i = 0 ; i < lista.length ; i++) 
+                    {
+                        if (lista.item(i).selected ) {
+                            if (sintomas == ""){ 
+                                sintomas +=lista.item(i).value;
+                            }else{
+                                sintomas +="," +lista.item(i).value;
+                            }
                         }
-                }
-            }
-            var cad2 = sintomas;
-            console.log(cad2);
-                                
-            if( cad2.length > 0 )
-            {
-                console.log("Cadena" + cad2);
-                //Aquí se hace el llamado a un php con conexión a MySQL.
-                $http.get( 'llamado-php.php?cadena=' + cad2 ).success
-                (
-                function( response ) 
-                { 
-                    console.log( response );
-                    $scope.campos = response.records;            
-                }
-                );   
-            }                    
-        }
-
-
-      
-
-
-        /**
-        * Esta función se activa al usar el texto 2.
-        */
-        $scope.busqueda = function()
-        {
-            var lista= document.getElementById('busqueda');
-            console.log(lista.length );
-            var sintomas="";
-
-
-            for (var i = 0 ; i < lista.length ; i++) 
-            {
-                if (lista.item(i).selected ) {
-                    if (sintomas == ""){ 
-                        sintomas +=lista.item(i).value;
-                    }else{
-                        sintomas +="," +lista.item(i).value;
                     }
-                }
+                    var cad2 = sintomas;
+                    console.log(cad2);
+                
+                    if( cad2.length > 0 )
+                    {
+                        console.log("Cadena" + cad2);
+                        //Aquí se hace el llamado a un php con conexión a MySQL.
+                        $http.get( 'llamado-php.php?cadena=' + cad2 ).success
+                        (
+                            function( response ) 
+                            { 
+                                console.log( response );
+                                $scope.campos = response.records;            
+                            }
+                        );   
+                    }                    
             }
-            var cad1 = sintomas;
-            console.log(cad1);
 
-            if( cad1.length > 0 )
+
+            $scope.buscar = function()
             {
-                console.log("busqueda" + cad1);
+                var busqueda = $scope.text_busqueda;    
+                console.log(busqueda);
                 //Aquí se hace el llamado a un php con conexión a MySQL.
-                $http.get( 'traer_resultados.php?cadena=' + cad1 ).success
+                 $http.get( 'llamado-php.php?busqueda=' + busqueda ).success
                 (
                     function( response ) 
                     { 
@@ -84,8 +61,11 @@ acumuladorApp.controller( "acumuladorAppCtrl",
                         $scope.campos = response.records;            
                     }
                 );   
-            }                    
+                                       
+            }
+
         }
-    }
-] //Si se minifica, se deben minificar todas las llamadas inyecciones, de lo contrario mejor no minifique nada.
+    ] //Si se minifica, se deben minificar todas las llamadas inyecciones, de lo contrario mejor no minifique nada.
 );
+
+
