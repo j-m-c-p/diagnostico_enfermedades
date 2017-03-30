b1a6bc9aa68b8480d5445e2018921c96c05cf39f
j-m-c-p <jhonatancubi@gmail.com>
2017-03-30 16:03:22 -0500

    Update llamada-php.php

--- www/llamada-php.php
+++ www/llamada-php.php
@@ -6,15 +6,29 @@
      * incluso desde el administrador de este sitio.
      */
 
-     include'BD.php';
-     $nuevo_obj=new BD();    // llama la clase BD
+    
 
-     if( isset( $_GET[ 'cadena' ] ) )
-     {     
-         $valores=$_GET['cadena'];
-         echo  $nuevo_obj->consultar($valores);
-         //echo $sql;
-     }
 
- 
-?> 
+include'class/BD.php'; //Se incluye todas las funcione y clase que se encuentra en BD.php
+    $nuevo_obj=new BD();    // llama la clase BD
+           
+     if( isset( $_GET[ 'cadena' ] ) )//Recibe todo lo que contiene cadena y hace la respectiva consulta
+    {     
+        $valores=$_GET['cadena'];
+        echo  $nuevo_obj->consultar($valores);//Se trae la función consultar que se encuentra en el BD.php
+        //echo $sql;
+    }
+
+     if( isset( $_GET[ 'busqueda' ] ) )//Recibe todo loq ue contiene busqueda y hace una busqueda en la base de datos mediante la función
+    {  
+        if ($_GET['busqueda']!="") {
+           $valores=$_GET['busqueda'];
+        echo  $nuevo_obj->buscar();//Se trae la función buscar que se encuentra en el BD.php
+        //echo $sql;
+        }
+        
+    }
+
+
+
+?>
