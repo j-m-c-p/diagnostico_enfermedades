ef792a11639618fd0ff68188b8c1926e07cf31b9
j-m-c-p <jhonatancubi@gmail.com>
2017-03-30 15:59:51 -0500

    Update nuevo.js

--- www/js/nuevo.js
+++ www/js/nuevo.js
@@ -1,71 +1,72 @@
-var acumuladorApp = angular.module( 'acumuladorApp', [] );
-acumuladorApp.controller( "acumuladorAppCtrl",
-    
-    //[ "$scope",  //Originalmente solo se minificaba el scope.
-    [ "$scope", "$http", //Esto es para minificar, pero interfiere con lo de php, hay que minificar las otras variables.
-     
-        function( $scope, $http )
-        {
+ var acumuladorApp = angular.module( 'acumuladorApp', [] );
+            
+    acumuladorApp.controller( "acumuladorAppCtrl",
         
-            
-            
-            /**
-             * Esta función se activa al usar el texto 2.
-             */
-            $scope.cargar_datos_php = function()
+        //[ "$scope",  //Originalmente solo se minificaba el scope.
+        [ "$scope", "$http", //Esto es para minificar, pero interfiere con lo de php, hay que minificar las otras variables.
+         
+            function( $scope, $http )
             {
-                    var lista= document.getElementById('datos');
-                    console.log(lista.length );
-                    var sintomas="";
+            
+                
+                
+               
+                /**
+                *esta función sirve para traer todas las enfermedades según los signos y síntomas y luego mostrarlo en index.php
+                */
+                $scope.cargar_datos_php = function()
+                {
+                        var lista= document.getElementById('datos');
+                        console.log(lista.length );
+                        var sintomas="";
 
 
-                    for (var i = 0 ; i < lista.length ; i++) 
-                    {
-                        if (lista.item(i).selected ) {
-                            if (sintomas == ""){ 
-                                sintomas +=lista.item(i).value;
-                            }else{
-                                sintomas +="," +lista.item(i).value;
+                        for (var i = 0 ; i < lista.length ; i++) 
+                        {
+                            if (lista.item(i).selected ) {
+                                if (sintomas == ""){ 
+                                    sintomas +=lista.item(i).value;
+                                }else{
+                                    sintomas +="," +lista.item(i).value;
+                                }
                             }
                         }
-                    }
-                    var cad2 = sintomas;
-                    console.log(cad2);
-                
-                    if( cad2.length > 0 )
-                    {
-                        console.log("Cadena" + cad2);
-                        //Aquí se hace el llamado a un php con conexión a MySQL.
-                        $http.get( 'llamado-php.php?cadena=' + cad2 ).success
-                        (
-                            function( response ) 
-                            { 
-                                console.log( response );
-                                $scope.campos = response.records;            
-                            }
-                        );   
-                    }                    
+                        var cad2 = sintomas;
+                        console.log(cad2);
+                    
+                        if( cad2.length > 0 )
+                        {
+                            console.log("Cadena" + cad2);
+                            //Aquí se hace el llamado a un php con conexión a MySQL.
+                            $http.get( 'llamado-php.php?cadena=' + cad2 ).success
+                            (
+                                function( response ) 
+                                { 
+                                    console.log( response );
+                                    $scope.campos = response.records;            
+                                }
+                            );   
+                        }                    
+                }
+
+                /**
+                *esta función sirve para traer lo que se ha buscado y luego mostrarlo en ayuda.php
+                */
+                $scope.buscar = function()
+                {
+                    var busqueda = $scope.text_busqueda;    
+                    console.log(busqueda);
+                    //Aquí se hace el llamado a un php con conexión a MySQL.
+                     $http.get( 'llamado-php.php?busqueda=' + busqueda ).success
+                    (
+                        function( response ) 
+                        { 
+                            console.log( response );
+                            $scope.campos = response.records;            
+                        }
+                    );                                        
+                }
+
             }
-
-
-            $scope.buscar = function()
-            {
-                var busqueda = $scope.text_busqueda;    
-                console.log(busqueda);
-                //Aquí se hace el llamado a un php con conexión a MySQL.
-                 $http.get( 'llamado-php.php?busqueda=' + busqueda ).success
-                (
-                    function( response ) 
-                    { 
-                        console.log( response );
-                        $scope.campos = response.records;            
-                    }
-                );   
-                                       
-            }
-
-        }
-    ] //Si se minifica, se deben minificar todas las llamadas inyecciones, de lo contrario mejor no minifique nada.
-);
-
-
+        ] //Si se minifica, se deben minificar todas las llamadas inyecciones, de lo contrario mejor no minifique nada.
+    );
