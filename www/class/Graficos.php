9a82c99fd9c548889d3b57a462cba27235f46990
j-m-c-p <jhonatancubi@gmail.com>
2017-04-04 10:07:02 -0500

    Update Graficos.php

--- www/class/Graficos.php
+++ www/class/Graficos.php
@@ -14,37 +14,46 @@ class Graficos
 {
 		/**
 		*esta función contiene el link de la libreria de bootstrap. 
-		*
+		*@return 		caracteres 		retorna estilos de bootstrap.
 		*/
 		function estilos($carpeta=null)
-			{
-				$salida="";
-				$salida=" <link rel='stylesheet'  href='css/bootstrap/bootstrap.min.css'>";
-						 
-				return $salida;		 
-			}
-				/**
-			* Esta función se encarga de retornar el encabezado.
-			*
-			*/
-		function encabezado()
-			{
-				$salida="";
-				$salida="<img class='img img-responsive' src='imagenes/encabezado.png'>";
-				return $salida;
-			}
+		{
+			$salida="";
+			$salida=" <link rel='stylesheet'  href='css/bootstrap/bootstrap.min.css'>";
+					 
+			return $salida;		 
+		}
 
+		/**
+		*esta función contiene el encabezado de la página. 
+		*@return 		caracteres 		retorna el encabezado.
+		*/
+		function encabezado()
+		{
+			$salida="";
+			$salida="<img class='img img-responsive' src='imagenes/encabezado.png'>";
+			return $salida;
+		}
+		/**
+		*esta función nos envia a un formulario de busqueda. 
+		*@return 		caracteres 		icono de ayuda.
+		*/
 		function ayuda()
-			{
-				$salida="";
-				$salida="<a href='ayuda.php'><img src='imagenes/help.png' align='right'></a>";
-				return $salida;
-			}
+		{
+			$salida="";
+			$salida="<a href='ayuda.php'><img src='imagenes/help.png' align='right'></a>";
+			return $salida;
+		}
+		/**
+		*esta función nos permi8te dirijirnos al index. 
+		*@return 		caracteres 		icono de inicio.
+		*/
 		function inicio()
-			{
-				$salida="";
-				$salida="<a href='index.php'><img src='imagenes/inicio.png' align='right' class='img img-responsive'></a>";
-				return $salida;
-			}
+		{
+			$salida="";
+			$salida="<a href='index.php'><img src='imagenes/inicio.png' align='right' class='img img-responsive'></a>";
+			return $salida;
+		}
 
 }
+?>
