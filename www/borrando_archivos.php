ef1a91bed4fc4d7df07b06667a321fae9715cae5
j-m-c-p <jhonatancubi@gmail.com>
2017-04-27 17:44:18 -0500

    Update borrando_archivos.php

--- www/borrando_archivos.php
+++ www/borrando_archivos.php
@@ -1,15 +1,14 @@
 <?php
 
     /**
-    * Autor: Camilo Figueroa ( Crivera )
+    * Autores: Jhonnatan Cubides - Harley Santoyo
     * Este código borra los archivos de la instalación y redirige al sitio. 
     */
 
-    include( "class/Verificador.php" );
-    $objeto_verificador = new Verificador();
+    include( "class/Verificador.php" );//se incluye la clase verificador 
+    $objeto_verificador = new Verificador();//se nombra una variable para crear un nuevo objeto.
 
-    $objeto_verificador->borrar_archivo( "instalador.php" );
-    $objeto_verificador->borrar_archivo( "instalando.php" );
-    header( "location: index.php" );
-
+    $objeto_verificador->borrar_archivo( "instalador.php" );//Con  esta linea de código el archivo instalador.php se elimina.
+    $objeto_verificador->borrar_archivo( "instalando.php" );//Con  esta linea de código el archivo instalando.php se elimina.
+    header( "location: index.php" );//luego de que se eliminen los archivos nos direcciona a index.php.
 ?>
